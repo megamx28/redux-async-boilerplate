824492dffbe41b55685cc4c4ef5aac53d9cf13db
Shane Da Silva <shanemichaeldasilva@gmail.com>
2015-12-13 14:37:22 -0800

    Add tests folder and react addons package

---
+++ tests/components/TestPage_spec.jsx
@@ -0,0 +1,13 @@
+import React    from 'react/addons'
+import {expect} from 'chai'
+import {
+    renderIntoDocument,
+    scryRenderedDOMComponentsWithTag
+} from 'react-addons-test-utils'
+
+describe('Voting', () => {
+    it('is a test', () => {
+        const testBool = true
+        expect(testBool).to.equal(true)
+    })
+})
