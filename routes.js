4ded9607b658edf46cc76d07e4898f7b3347f6bb
Shane Da Silva <shanemichaeldasilva@gmail.com>
2015-12-14 14:33:50 -0800

    Add default route

--- routes.js
+++ routes.js
@@ -1,10 +1,11 @@
-import React     from 'react'
-import { Route } from 'react-router'
-import App       from './containers/App'
-import TestPage  from './views/TestPage'
+import React                 from 'react'
+import { Route, IndexRoute } from 'react-router'
+import App                   from './containers/App'
+import TestPage              from './views/TestPage'
 
 export default (
     <Route path="/" component={App}>
+        <IndexRoute component={TestPage} />
         <Route path="/testing" component={TestPage} />
     </Route>
 )
