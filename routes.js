74ee754c11ccf669c63f89959ecc7ad66a57f49e
Shane Da Silva <shanemichaeldasilva@gmail.com>
2015-12-14 14:27:08 -0800

    Move TestPage into views folder

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 import React     from 'react'
 import { Route } from 'react-router'
 import App       from './containers/App'
-import TestPage  from './containers/TestPage'
+import TestPage  from './views/TestPage'
 
 export default (
     <Route path="/" component={App}>
---
+++ views/TestPage.js
@@ -0,0 +1,60 @@
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators }          from 'redux'
+import { connect }                     from 'react-redux'
+import UserActions                     from '../actions/users'
+
+class TestPage extends Component {
+    propTypes: {
+        actions: React.PropTypes.object,
+        users: PropTypes.object
+    }
+
+    constructor(props) {
+        super(props)
+
+        this.renderUsers = this.renderUsers.bind(this)
+    }
+
+    componentWillMount() {
+        this.props.actions.loadUsers()
+    }
+
+    renderUsers() {
+        return (
+            <ol>
+                {this.props.users.map((user, index) => {
+                    return <li key={index}>{user.name}</li>
+                })}
+            </ol>
+        )
+    }
+
+    render() {
+        if (!this.props.users.length) {
+            return (
+                <div>
+                    <h1><i>Loading user...</i></h1>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                {this.renderUsers()}
+            </div>
+        )
+    }
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+        users: state.users
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    actions: bindActionCreators(UserActions, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TestPage)
