c3e8158234e38330e33dffe9391ba7d1aeaf8e92
Shane Da Silva <shanemichaeldasilva@gmail.com>
2015-12-22 15:53:27 -0800

    Fix test suite issues

---
+++ tests/components/FilterInput.spec.js
@@ -0,0 +1,15 @@
+import React       from 'react'
+import {expect}    from 'chai'
+import {Map, List} from 'immutable'
+import {
+    renderIntoDocument,
+    scryRenderedDOMComponentsWithTag,
+    Simulate
+} from 'react-addons-test-utils'
+
+describe('Test', () => {
+    it('is true', () => {
+        expect(1).to.equal(1)
+    })
+})
+
