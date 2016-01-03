31a5ff515e2e72274c122d98ad27db105f7cb950
Shane Da Silva <shanemichaeldasilva@gmail.com>
2016-01-03 10:32:52 -0800

    Add bin folder

---
+++ bin/server.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack')
+var WebpackDevServer = require('webpack-dev-server')
+var config = require('./../webpack.hot.config')
+
+const debug = require('debug')('app:server')
+
+debug('Creating Webpack Dev Server.')
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    quiet: true,
+    colors: true,
+    stats: { colors: true },
+    historyApiFallback: true
+}).listen(3000, 'localhost', function (err, result) {
+    if (err) {
+        debug('There was an error creating Webpack Dev Server: ' + err)
+    }
+
+    debug('Listening at localhost:3000')
+})
