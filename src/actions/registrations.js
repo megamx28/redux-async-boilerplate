309e79fa8cf366c24770f1c94cc86c18827f8ae6
Shane Da Silva <shane.dasilva@hootsuite.com>
2016-07-14 15:32:06 -0700

    add sessions and registrations async actions

--- src/actions/registrations.js
+++ src/actions/registrations.js
@@ -1,8 +1,15 @@
-import { push }           from 'react-router-redux';
-import Constants          from '../constants';
-import { setCurrentUser } from './sessions';
+import { push }     from 'react-router-redux';
+import { CALL_API } from 'redux-api-middleware';
 
 export default {
-    signUp: (data) => {
-    }
+	signUp: (data) => {
+		return {
+			[CALL_API]: {
+				endpoint: '/api/v1/registrations',
+				method: 'POST',
+				body: data,
+				types: ['REQUEST', 'SUCCESS', 'FAILURE']
+			}
+		}
+	}
 }
--- src/actions/sessions.js
+++ src/actions/sessions.js
@@ -1,14 +1,39 @@
-import { push }  from 'react-router-redux';
-import Constants from '../constants';
-import CALL_API  from '../constants/callApi'
+import { push }     from 'react-router-redux';
+import Constants    from '../constants';
+import { CALL_API } from 'redux-api-middleware';
 
 export default {
-    signIn: (email, password) => {
-    },
+	signIn: (email, password) => {
+		return {
+			[CALL_API]: {
+				endpoint: '/api/v1/sessions',
+				method: 'POST',
+				body: {
+					email: email,
+					password: password
+				},
+				types: ['REQUEST', 'SUCCESS', 'FAILURE']
+			}
+		}
+	},
 
-    currentUser: () => {
-    },
+	currentUser: () => {
+		return {
+			[CALL_API]: {
+				endpoint: '/api/v1/current-user',
+				method: 'GET',
+				types: ['REQUEST', 'SUCCESS', 'FAILURE']
+			}
+		}
+	},
 
-    signOut: () => {
-    }
+	signOut: () => {
+		return {
+			[CALL_API]: {
+				endpoint: '/api/v1/sessions',
+				method: 'DELETE',
+				types: ['REQUEST', 'SUCCESS', 'FAILURE']
+			}
+		}
+	}
 }
